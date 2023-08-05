a987ec1468a9561370fd2c5f227f4691b2eb5715
Pierre-LouisDeu <73912685+Pierre-LouisDeu@users.noreply.github.com>
2023-08-05 20:32:59 +0200

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Redeployed
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
